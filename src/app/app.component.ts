2066862956d93402045e06522dc43e22ff65928c
ViliusVed <vilius.vedegys@gmail.com>
2022-11-30 01:49:19 +0200

    setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'loginScss';
-}
+export class AppComponent  {
+  private isDark = false;
+
+  @HostBinding('class')
+  get themeMode(){
+    return this.isDark ? 'theme-dark' : 'theme-light'
+  }
+  }
+  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { ThemeSwitchComponent } from './theme-switch/theme-switch.component';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from "@angular/material/icon";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatToolbarModule } from "@angular/material/toolbar"
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ThemeSwitchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatButtonToggleModule,
+    MatIconModule,
+    MatMenuModule,
+    MatToolbarModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
